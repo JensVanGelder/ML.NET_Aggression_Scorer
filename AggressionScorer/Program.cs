b414be894d9977e4f5ba66da2866b87cbe04dd22
JensVanGelder <jens.v.gelder@gmail.com>
2021-06-09 19:21:32 +0200

    Retraining the model with split test & train data

--- AggressionScorer/Program.cs
+++ AggressionScorer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using AggressionScorerModel;
 using Microsoft.ML;
+using Microsoft.ML.Calibrators;
+using Microsoft.ML.Trainers;
 using System;
 using System.IO;
 
@@ -11,25 +13,12 @@ namespace AggressionScorer
         {
             Console.WriteLine("Aggression scorer model builder started.");
 
-            var mlContext = new MLContext();
-            var inputDataPreparer = mlContext
-                .Transforms
-                .Text
-                .FeaturizeText("Features", "Comment")
-                .AppendCacheCheckpoint(mlContext);
-
-            //Create training algorithm
-            Console.WriteLine("Creatong algorithm");
-
-            var trainer = mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression();
-
-            var trainingPipeline = inputDataPreparer.Append(trainer);
-
+            var mlContext = new MLContext(0);
             //Load data
             Console.WriteLine("Loading data");
 
             var createdInputFile = @"Data\preparedInput.tsv";
-            DataPreparer.CreatePreparedDataFile(createdInputFile, onlySaveSmallSubset: false);
+            DataPreparer.CreatePreparedDataFile(createdInputFile, onlySaveSmallSubset: true);
 
             IDataView trainingDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
                 path: createdInputFile,
@@ -38,12 +27,36 @@ namespace AggressionScorer
                 allowQuoting: true
                 );
 
+            var inputDataSplit = mlContext.Data.TrainTestSplit(trainingDataView, testFraction: .2, seed: 0);
+
+            //Build pipeline
+            var inputDataPreparer = mlContext
+                .Transforms
+                .Text
+                .FeaturizeText("Features", "Comment")
+                .Append(mlContext.Transforms.NormalizeMeanVariance("Features"))
+                .AppendCacheCheckpoint(mlContext)
+                .Fit(inputDataSplit.TrainSet);
+
+            //Create training algorithm
+            Console.WriteLine("Creating algorithm");
+
+            var trainer = mlContext
+                .BinaryClassification
+                .Trainers
+                .LbfgsLogisticRegression();
+
             //Fit model
+            Console.WriteLine("Training model");
+
             var startTime = DateTime.Now;
-            ITransformer model = trainingPipeline.Fit(trainingDataView);
+            var transformedData = inputDataPreparer.Transform(inputDataSplit.TrainSet);
+            ITransformer model = trainer.Fit(transformedData);
+
             Console.WriteLine($"Model training finished in {(DateTime.Now - startTime).TotalSeconds} seconds");
 
             //Test model
+            EvaluateModel(mlContext, model, inputDataPreparer.Transform(inputDataSplit.TestSet));
 
             //Save model
             Console.WriteLine("Saving the model");
@@ -56,6 +69,88 @@ namespace AggressionScorer
             mlContext.Model.Save(model, trainingDataView.Schema, modelFile);
 
             Console.WriteLine($"Model is saved to {modelFile}");
+
+            var dataPreparePipelineFile = "dataPreparePipeline.zip";
+
+            Console.WriteLine("Saving the input data preparing pipeline");
+
+            mlContext.Model.Save(inputDataPreparer, trainingDataView.Schema, dataPreparePipelineFile);
+
+            Console.WriteLine($"The pipeline is saved to {dataPreparePipelineFile}");
+
+            var retrainedModel = RetrainModel(modelFile, dataPreparePipelineFile);
+
+            var completeRetrainedPipeline = inputDataPreparer.Append(retrainedModel);
+
+            Console.WriteLine($"Saving the retrained model");
+            string retrainedModelFile = @"Model\\AggressionScoreRetrainedModel.zip";
+
+            mlContext.Model.Save(completeRetrainedPipeline, trainingDataView.Schema, retrainedModelFile);
+
+            Console.WriteLine("The model is saved to {0}", retrainedModelFile);
+
+            EvaluateModel(mlContext, completeRetrainedPipeline, inputDataSplit.TestSet);
+        }
+
+        private static ITransformer RetrainModel(string modelFile, string dataPreparePipelineFile)
+        {
+            MLContext mlContext = new MLContext(0);
+
+            // Load pre trained model
+            ITransformer pretrainedModel = mlContext.Model.Load(modelFile, out _);
+
+            // Extract pretrained model parameters
+            var pretrainedModelParameters =
+                ((ISingleFeaturePredictionTransformer
+                    <CalibratedModelParametersBase<LinearBinaryModelParameters, PlattCalibrator>>)
+                    pretrainedModel)
+                .Model.SubModel;
+
+            string dataFile = @"Data\preparedInput.tsv";
+            DataPreparer.CreatePreparedDataFile(dataFile, onlySaveSmallSubset: false);
+
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
+                path: dataFile,
+                hasHeader: true,
+                separatorChar: '\t',
+                allowQuoting: true
+            );
+
+            // Load data preparation pipeline
+            ITransformer dataPrepPipeline = mlContext.Model.Load(dataPreparePipelineFile, out _);
+
+            // Prepare input data to a form consumable by a machine learning model
+            var newData = dataPrepPipeline.Transform(trainingDataView);
+
+            // Retrain model
+            Console.WriteLine("Start retraining model");
+
+            var startTime = DateTime.Now;
+
+            var retrainedModel =
+                mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression()
+                    .Fit(newData, pretrainedModelParameters);
+
+            Console.WriteLine($"Model retraining finished in {(DateTime.Now - startTime).TotalSeconds} seconds");
+
+            return retrainedModel;
+        }
+
+        private static void EvaluateModel(MLContext mlContext, ITransformer trainedModel, IDataView testData)
+        {
+            Console.WriteLine();
+            Console.WriteLine("-- Evaluating binary classification model performance --");
+            Console.WriteLine();
+
+            var predictedData = trainedModel.Transform(testData);
+            var metrics = mlContext.BinaryClassification.Evaluate(predictedData);
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:0.###}");
+
+            Console.WriteLine();
+            Console.WriteLine("Confusion Matrix");
+            Console.WriteLine();
+            Console.WriteLine(metrics.ConfusionMatrix.GetFormattedConfusionTable());
+            Console.WriteLine();
         }
     }
 }
--- AggressionScorerModel/AggressionScoreServiceExtensions.cs
+++ AggressionScorerModel/AggressionScoreServiceExtensions.cs
@@ -12,7 +12,7 @@ namespace AggressionScorerModel
     public static class AggressionScoreServiceExtensions
     {
         private static readonly string _modelFile =
-            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Model", "AggressionScoreModel.zip");
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Model", "AggressionScoreRetrainedModel.zip");
 
         public static void AddAggressionScorerPredictionEnginePool(this IServiceCollection services)
         {
