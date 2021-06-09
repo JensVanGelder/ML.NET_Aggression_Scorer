0fbb5b220835da71636109dcdbabc1ea072c882e
JensVanGelder <jens.v.gelder@gmail.com>
2021-06-09 17:08:50 +0200

    Add project files.

---
+++ AggressionScorer/Program.cs
@@ -0,0 +1,61 @@
+﻿using AggressionScorerModel;
+using Microsoft.ML;
+using System;
+using System.IO;
+
+namespace AggressionScorer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Aggression scorer model builder started.");
+
+            var mlContext = new MLContext();
+            var inputDataPreparer = mlContext
+                .Transforms
+                .Text
+                .FeaturizeText("Features", "Comment")
+                .AppendCacheCheckpoint(mlContext);
+
+            //Create training algorithm
+            Console.WriteLine("Creatong algorithm");
+
+            var trainer = mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression();
+
+            var trainingPipeline = inputDataPreparer.Append(trainer);
+
+            //Load data
+            Console.WriteLine("Loading data");
+
+            var createdInputFile = @"Data\preparedInput.tsv";
+            DataPreparer.CreatePreparedDataFile(createdInputFile, onlySaveSmallSubset: false);
+
+            IDataView trainingDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
+                path: createdInputFile,
+                hasHeader: true,
+                separatorChar: '\t',
+                allowQuoting: true
+                );
+
+            //Fit model
+            var startTime = DateTime.Now;
+            ITransformer model = trainingPipeline.Fit(trainingDataView);
+            Console.WriteLine($"Model training finished in {(DateTime.Now - startTime).TotalSeconds} seconds");
+
+            //Test model
+
+            //Save model
+            Console.WriteLine("Saving the model");
+
+            if (!Directory.Exists("Model"))
+            {
+                Directory.CreateDirectory("Model");
+            }
+            var modelFile = @"Model\\AggressionScoreModel.zip";
+            mlContext.Model.Save(model, trainingDataView.Schema, modelFile);
+
+            Console.WriteLine($"Model is saved to {modelFile}");
+        }
+    }
+}
---
+++ SentimentDetection/Controllers/AggressionScoreController.cs
@@ -0,0 +1,49 @@
+﻿using AggressionScorerModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SentimentDetection.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AggressionScoreController : ControllerBase
+    {
+        private readonly AggressionScorer _aggressionScorer;
+
+        public AggressionScoreController(AggressionScorer aggressionScorer)
+        {
+            _aggressionScorer = aggressionScorer;
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] CommentWrapper input)
+        {
+            return new JsonResult(ScoreComment(input.Comment));
+        }
+
+        private AggressionPrediction ScoreComment(string comment)
+        {
+            var classification = _aggressionScorer.Predict(comment);
+
+            return new AggressionPrediction()
+            {
+                IsAggressive = classification.Prediction,
+                Probability = classification.Probability
+            };
+        }
+    }
+
+    public class AggressionPrediction
+    {
+        public bool IsAggressive { get; set; }
+        public float Probability { get; set; }
+    }
+
+    public class CommentWrapper
+    {
+        public string Comment { get; set; }
+    }
+}
